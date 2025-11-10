a990223debdf798bf7d0439e7551e6bfa041fbc8
gabrielsoares2007souza <gabrielsoares2007souza@gmail.com>
2025-11-10 16:29:51 -0400

    aprendendo condicional simples e composta

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 80
+console.log(`Sua velocidade é de ${vel} km/h`)
+if(vel>40){
+    console.log(`Atenção!!!, Voçê está acima da velocidade permitida.`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Voçê está no país ${pais}`)
+if (pais!='brasil'){
+    console.log(`Voçê é estrangeiro`)
+}else{
+    console.log(`Voçê é brasileiro`)
+}
