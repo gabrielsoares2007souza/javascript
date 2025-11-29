ad92be0e6932b047ae00656ab9c790e2dac6d09a
gabrielsoares2007souza <gabrielsoares2007souza@gmail.com>
2025-11-29 15:22:09 -0400

    desafiofinal

---
+++ aula16ex/desafiofinal/scripts/script.js
@@ -0,0 +1,12 @@
+const adicionar = document.getElementById('add')
+const verificar = document.querySelector('#check')
+const res = document.querySelector("#res")
+
+adicionar.addEventListener('click', () =>{
+    const numero = document.querySelector("icont")
+    if(numero.value.length == 0 ){
+        alert("Há um problema!")
+    }else{
+        alert()
+    }
+})
