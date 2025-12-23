33d26c74249a0eae3350f5c1b5df7befbc6c2fb9
gabrielsoares2007souza <gabrielsoares2007souza@gmail.com>
2025-12-23 15:27:18 -0400

    metodo Toggle

---
+++ aula32/b/toggle/scripts/script.js
@@ -0,0 +1,112 @@
+let cursos = ['HTML' , 'CSS' , "PHP" , "MySQL" , 'JavaScript' , 'Java']
+const caixa2 = document.querySelector('#caixa2')
+const adicionarAntes = document.getElementById('adicionarAntes')
+const adicionarDepois = document.getElementById('adicionarDepois')
+const adicionar = document.getElementById('adicionar')
+const remover = document.getElementById('remover')
+const selecionado = document.getElementById('selecionado')
+const texto = document.querySelector('#itext')
+
+// const radio = ()=>{
+//     const inputsRadio = [...document.querySelectorAll('input[type=radio]')]
+//     const radioSelecionado = inputsRadio.filter((elem)=>{
+//         return elem.checked
+//     })
+//     if(radioSelecionado[0] != undefined){
+//         const opçaoEscolhida = radioSelecionado[0]
+
+//         const cursoSelecionado = opçaoEscolhida.parentNode.previousSibling
+        
+//         return cursoSelecionado
+//     }else{
+//         alert('Seleciona algum curso!')
+//     }
+// }
+const retirarSelecao = ()=>{
+    const cursosQueForamSelecionados = [...document.querySelectorAll('.selecionado')]
+
+    cursosQueForamSelecionados.map((el)=>{
+        el.classList.remove('selecionado')
+    })
+    return cursosQueForamSelecionados[0]
+}
+
+const criarElement = (elemento)=>{
+    const newElement = document.createElement('div')
+    newElement.setAttribute('class' , 'cursos')
+    newElement.innerHTML = elemento
+
+    newElement.addEventListener('click' , (evt)=>{
+        retirarSelecao()
+        newElement.classList.toggle('selecionado')
+    })
+    return newElement
+}
+cursos.map((el)=>{
+
+    const retornoCriacao = criarElement(el)
+    
+
+    caixa2.appendChild(retornoCriacao)
+})
+
+
+selecionado.addEventListener('click' , ()=>{
+    const retornoParaAlert = retirarSelecao()
+    if(retornoParaAlert!= undefined){
+       alert(`Curso Selecionado: ${retornoParaAlert.innerHTML}`)
+    }else{
+        alert('selecione algum curso!!')
+    }
+})
+
+remover.addEventListener("click" , ()=>{
+    const retornoParaRemover = retirarSelecao()
+    if( retornoParaRemover != undefined){
+        const blocoEliminar = retornoParaRemover
+        blocoEliminar.remove()
+    }else{
+        alert('Selecione algum curso!!')
+    }
+})
+
+adicionarAntes.addEventListener('click', ()=>{
+    if(texto.value != ''){
+        const retornoDaCaixaTexto = criarElement(texto.value)
+
+        const rad = retirarSelecao()
+        const nodeSelecionado = rad
+        if(nodeSelecionado != undefined){
+            caixa2.insertBefore(retornoDaCaixaTexto , nodeSelecionado )
+        }else{
+            alert('selecione algum curso')
+        }
+    }else{
+        alert("Digite algo na caixa de texto para adicionar")
+    }
+})
+
+adicionarDepois.addEventListener('click' , ()=>{
+    if(texto.value != ''){
+        const retornoDaCaixaTexto = criarElement(texto.value)
+
+        const rad = retirarSelecao()
+        const nodeSelecionado = rad
+        if(nodeSelecionado != undefined){
+            caixa2.insertBefore(retornoDaCaixaTexto , nodeSelecionado.nextSibling )
+        }else{
+            alert('Selecione algum curso!')
+        }
+    }else{
+        alert('Digite algo na caixa de texto para adicionar')
+    }
+})
+
+adicionar.addEventListener('click' , ()=>{
+    if(texto.value != ''){
+        const retornoDaCaixaTexto = criarElement(texto.value)
+        caixa2.appendChild(retornoDaCaixaTexto)
+    }else{
+        alert('digite algo')
+    }
+})
