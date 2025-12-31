dfaf133dbe5fdf45dd881e3307b6e0c68aa9a86b
gabrielsoares2007souza <gabrielsoares2007souza@gmail.com>
2025-12-31 15:39:44 -0400

    coleções map e set

---
+++ aula39/script.js
@@ -0,0 +1,8 @@
+const caixa = document.querySelector('#caixa')
+
+let colecao = new Map()
+
+colecao.set(0 , 'oi')
+colecao.set('ere' , 'ok')
+
+console.log(colecao)
---
+++ aula40/script.js
@@ -0,0 +1,8 @@
+const caixa = document.querySelector('#caixa')
+
+let esportes = new Set()
+
+esportes.add(2)
+esportes.add('gabriel')
+esportes.add('oi')
+console.log(esportes)
